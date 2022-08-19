ba4c545d02519adf4565c2e31db5500822f9a1e2
jthan jthan@enliple.com <jthan@enliple.com>
2022-08-19 12:27:00 +0900

    itemReader 학습

---
+++ src/main/java/com/example/springbatch/job/JdbcCursorItemReaderJobConfiguration.java
@@ -0,0 +1,97 @@
+package com.example.springbatch.job;
+
+import com.example.springbatch.domain.entity.Pay;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JdbcCursorItemReaderJobConfiguration {
+
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+	private final DataSource dataSource; // DataSource DI
+
+	private static final int chunkSize = 10;
+
+	@Bean
+	public Job jdbcCursorItemReaderJob() {
+		return jobBuilderFactory.get("jdbcCursorItemReaderJob")
+			.start(jdbcCursorItemReaderStep())
+			.build();
+	}
+
+	private Step jdbcCursorItemReaderStep() {
+		return stepBuilderFactory.get("jdbcCursorItemReaderStep")
+			.<Pay, Pay>chunk(chunkSize)
+			.reader(jdbcCursorItemReader())
+			.writer(jdbcCursorItemWriter())
+			.build();
+	}
+
+	private JdbcCursorItemReader<Pay> jdbcCursorItemReader() {
+		return new JdbcCursorItemReaderBuilder<Pay>()
+			.fetchSize(chunkSize)
+			.dataSource(dataSource)
+			.rowMapper(new BeanPropertyRowMapper<>(Pay.class))
+			.sql("SELECT id, amount, tx_name, tx_date_time FROM pay")
+			.name("jdbcCursorItemReader")
+			.build();
+	}
+
+	private ItemWriter<Pay> jdbcCursorItemWriter() {
+		return list -> {
+			for (Pay pay : list) {
+				log.info("Current Pay = {}", pay);
+			}
+		};
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
